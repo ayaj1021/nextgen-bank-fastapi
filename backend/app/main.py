f66fd714ba86a3540b7934518e46554eaf660fc1
Ayodele Ajayi <ayaj1021@gmail.com>
2026-05-15 22:39:10 +0100

    chore: create basic fastapi server

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-    title="NextGen Bank - FastAPI"
+    title="NextGen Bank - FastAPI",
+    description="Fully features banking API bilt with fastapi"
 )
+
+@app.get('/')
+def home():
+    return {"message": "Welcome to the Nextgen Bank"}

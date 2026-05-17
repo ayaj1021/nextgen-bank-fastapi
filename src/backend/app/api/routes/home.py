cb5d72d83745ea77a589c8fc71a4758ed665b781
Ayodele Ajayi <ayaj1021@gmail.com>
2026-05-17 05:02:15 +0100

    feat: add logging with loguru

--- src/backend/app/api/routes/home.py
+++ src/backend/app/api/routes/home.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
+from src.backend.app.core.logging import get_logger
 
-router = APIRouter(
-    prefix='/home'
+logger = get_logger()
 
-)
+router = APIRouter(prefix="/home")
+
 
 @router.get("/")
 def home():
+    logger.info("Home page accessed")
+    logger.debug("Home page accessed")
+    logger.error("Home page accessed")
+    logger.warning("Home page accessed")
+    logger.critical("Home page accessed")
     return {"message": "Welcome to the Nextgen Bank"}

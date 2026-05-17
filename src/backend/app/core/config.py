ae7a91291a688bcce3a3ff2d120fae3eb908773c
Ayodele Ajayi <ayaj1021@gmail.com>
2026-05-17 04:29:52 +0100

    chore: add env variables and config settings

---
+++ src/backend/app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Literal
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
+print(Path(__file__).resolve()) 
+class Settings(BaseSettings):
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
+    model_config = SettingsConfigDict(
+        # env_file="../../.envs/.env.local",
+        env_file=BASE_DIR / ".envs" / ".env.local",
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+    API_V1_STR: str = ""
+    PROJECT_NAME: str = ""
+    PROJECT_DESCRIPTION: str = ""
+    SITE_NAME: str = ""
+
+    
+
+settings = Settings()
